5b04be0831473d231e33b912e64a30890e32a064
Annabelf11 <fergusonannabel@gmail.com>
2016-05-30 18:04:02 -0400

    Fixed Ruby Review Lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
-def division(num1, num2)
- 
+
+def division(num1=42, num2=7)
+  total = 42 / 7
 end
 
-def assign_variable(value)
-  
+def assign_variable(name=bob)
+  "#{name}"
 end
 
-def argue
-  
+def argue(phrase="I'm right and you are wrong!")
+  "#{phrase}"
 end
 
-def greeting
-
+def greeting(greeting,name)
+  greeting="Hi there,"
+  name="Bobby!"
+  "#{greeting} #{name}"
 end
 
-def return_a_value
-
+def return_a_value(a="Nice")
+  "#{a}"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(b="cheese")
+  "#{b}"
 end
